8c3afbd29d6358f51fef484bb0f5e62c88f6c060
Marcelo Gomez <marcelogomez321@gmail.com>
2023-06-27 20:41:48 +0100

    Remove unused method

--- src/token.rs
+++ src/token.rs
@@ -75,52 +75,6 @@ pub enum Token {
     EOF,
 }
 
-impl Token {
-    fn len(&self) -> usize {
-        match self {
-            Token::LeftParen => 1,
-            Token::RightParen => 1,
-            Token::LeftBrace => 1,
-            Token::RightBrace => 1,
-            Token::Comma => 1,
-            Token::Dot => 1,
-            Token::Minus => 1,
-            Token::Plus => 1,
-            Token::Semicolon => 1,
-            Token::Slash => 1,
-            Token::Star => 1,
-            Token::Bang => 1,
-            Token::BangEqual => 2,
-            Token::Equal => 1,
-            Token::EqualEqual => 2,
-            Token::Greater => 1,
-            Token::GreaterEqual => 2,
-            Token::Less => 1,
-            Token::LessEqual => 2,
-            Token::And => 3,
-            Token::Class => 5,
-            Token::Else => 4,
-            Token::False => 5,
-            Token::For => 3,
-            Token::Fun => 3,
-            Token::If => 2,
-            Token::Nil => 3,
-            Token::Or => 2,
-            Token::Print => 5,
-            Token::Return => 6,
-            Token::Super => 5,
-            Token::This => 4,
-            Token::True => 4,
-            Token::Var => 3,
-            Token::While => 5,
-            Token::EOF => 0,
-            Token::Identifier(iden) => iden.len(),
-            Token::String(str) => str.len() + 2,
-            Token::Number(num) => num.to_string().len(),
-        }
-    }
-}
-
 #[derive(Debug, PartialEq)]
 pub struct TokenPos {
     pub token: Token,
